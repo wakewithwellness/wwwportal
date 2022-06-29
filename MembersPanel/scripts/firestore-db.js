cc3a187ee7c386e261311f941c479317b866917e
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-06-29 13:24:07 +0530

    Update firestore-db.js

--- MembersPanel/scripts/firestore-db.js
+++ MembersPanel/scripts/firestore-db.js
@@ -171,11 +171,13 @@ async function getuserInfoRealtime(userID){
             </div>
 
 
+           
+
+
             <div class="container-fluid">
- 
   <div class="row" style="margin-top:-25px ;border: 1px solid #009efb; margin-bottom:10px ;color:white;height:35px">
-    <div class="col-8" style="padding-top:6px">Download your certificate </div>
-    <div onclick="location.href='certificates.html'" class="col-4" style="cursor:pointer;background-color: #009efb;padding-top:6px"><i class="fa fa-hand-pointer-o" aria-hidden="true"></i> </i>Click here</div>
+    <div class="col-8" style="padding-top:6px">Download our Android Portal App Now ! </div>
+    <div onclick="location.href='https://firebasestorage.googleapis.com/v0/b/files-bf645.appspot.com/o/WWW.apk?alt=media&token=a467a626-80d4-436c-afe5-0de4d2562559'" class="col-4" style="cursor:pointer;background-color: #009efb;padding-top:6px"><i class="fa fa-download" aria-hidden="true"></i> </i>Download</div>
   </div>
 </div>
            
