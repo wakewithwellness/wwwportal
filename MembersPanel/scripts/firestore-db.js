7220a642490892644adbc9d113886aaaec4150b5
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-07-02 22:23:57 +0530

    Update firestore-db.js

--- MembersPanel/scripts/firestore-db.js
+++ MembersPanel/scripts/firestore-db.js
@@ -67,7 +67,8 @@ async function getuserInfo(userID){
               <input type="password" id="loginPassword" placeholder="Password" style="width: 100%;
               height: 40px;padding-left: 10px;margin-bottom: 20px;background: #000;font-family: Montserrat;font-weight: 500;color: #fff;
               font-size: 12px; border-bottom: 2px solid transparent;border-top-left-radius: 2%; border-top-right-radius: 2%;border-color: cornflowerblue;">
-              <a href="#" onclick="forgotPass()">Forgot Password</a><br>
+              <a class="modal-trigger" href="#modal2">Forgot Password</a><br>
+              
               <div id="button_container" style="width: 100%;height: 45px;background-color: cornflowerblue;color: #fff;margin-top: 15px;" >
                   <button type="submit" style="width: 100%;height: 100%;background: transparent;color: inherit;font-family: Montserrat;letter-spacing: 1px;
                   font-weight: 900;font-size: 12px;cursor: pointer;align-items: center;">Login</button>
@@ -101,6 +102,8 @@ async function getuserInfoRealtime(userID){
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    
 
 
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
@@ -369,7 +372,7 @@ async function getuserInfoRealtime(userID){
                 <input type="password" id="loginPassword" placeholder="Password" style="width: 100%;
                 height: 40px;padding-left: 10px;margin-bottom: 20px;background: #000;font-family: Montserrat;font-weight: 500;color: #fff;
                 font-size: 12px; border-bottom: 2px solid transparent;border-top-left-radius: 2%; border-top-right-radius: 2%;border-color: cornflowerblue;">
-                <a href="#" onclick="forgotPass()">Forgot Password</a><br>
+                <a class="modal-trigger" href="#modal2">Forgot Password</a><br>
                 <div id="button_container" style="width: 100%;height: 45px;background-color: cornflowerblue;color: #fff;margin-top: 15px;" >
                 
                  <button type="submit" style="width: 100%;height: 100%;background: transparent;color: inherit;font-family: Montserrat;letter-spacing: 1px;
