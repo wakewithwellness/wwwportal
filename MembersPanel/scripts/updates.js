a7aebc6f898b8bfefe36f3737ec1a34b0863108d
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-08-27 21:07:42 +0530

    Update updates.js

--- MembersPanel/scripts/updates.js
+++ MembersPanel/scripts/updates.js
@@ -1,168 +1,333 @@
-var firebaseConfig = {
-     apiKey: "AIzaSyDoiXHxMG_vZPHz0eeJj5s661xhfU-Y4_4",
-     authDomain: "members-76725.firebaseapp.com",
-     projectId: "members-76725",
-     storageBucket: "members-76725.appspot.com",
-     messagingSenderId: "734645197873",
-     appId: "1:734645197873:web:808127fdb4d643d4d24910",
-     measurementId: "G-7LYZ3R48GT"
- };
- // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
- const db = firebase.firestore();
- db.settings({ timestampInSnapshots: true});
+const Atten  = document.querySelector('.Atten')
 
 
 
+function createUserCollection(user){
+   firebase.firestore().collection('users')
+   .doc(user.uid)
+   .set({
+       uid:user.uid,
+       name:user.displayName,
+       email:user.email,
+     
 
-// Batch1 (1st Year) batch
- db.collection("attendance").orderBy('regno').limit(32).get().then(querySnapshot=>{
- querySnapshot.forEach(doc=>{
-     let data = doc.data();
-     let row  = `<tr>
        
-                <th scope="row">
-                   <label>${data.regno}</label>
-                 </th>
-                     <td>${data.name}</td>
-                     <td style="text-align:center;font-weight:800;color: ${data.status1}">${data.p1}</td>
-                         <td>${data.May2}</td>
-                         <td>${data.May1}</td>
-                     <td>${data.Apr2}</td>
-                     <td>${data.Apr1}</td>
-                     <td>${data.Mar2}</td>
-                     <td>${data.Mar1}</td>
-                     <td>${data.Feb2}</td>
-                     <td>${data.Feb1}</td>
-                     <td>${data.Jan2}</td>
-                     <td>${data.Jan1}</td>
-                     <td>${data.Dec2}</td>
-                     <td>${data.Dec1}</td>
-                     <td>${data.Nov2}</td>
-                     <td>${data.Nov1}</td>
-                     <td>${data.Oct2}</td>
-                     <td>${data.Oct1}</td>
-                     <td>${data.Sept2}</td>
-                     <td>${data.Sept1}</td>
-                     <td>${data.Aug2}</td>
-                     <td>${data.Aug1}</td>
-                     <td>${data.July3}</td>
-                     <td>${data.July2}</td>
-                     <td>${data.July1}</td>
-               </tr>`;
-     let table = document.getElementById('myTable')
-     table.innerHTML += row
- })
-})
+
+   })
+}
 
 
+async function getuserInfo(userID){
+    if(userID){
+      const userInfoSnap = await  firebase.firestore()
+    .collection('users')
+    .doc(userID)
+    .get()
 
+   const userInfo = userInfoSnap.data()
+   if(userInfo){
+    Atten.innerHTML = `
+       <h3>${userInfo.name}</h3>
+       <h3>${userInfo.email}</h3>
 
-// Batch2 (1st Year) batch
-db.collection("attendance").orderBy('regno').startAt('22WWW0060').endAt('22WWW0066').get().then(querySnapshot=>{
-     querySnapshot.forEach(doc=>{
-         let data = doc.data();
-         let row  = `<tr>
+       `
+   }    
+    }else{
+        Atten.innerHTML = `<div id="content_container" class="container" style=" width: 47vh;
+      height: 43vh;margin-top:180px">
+      
+      <div id="form_container" style="width: 100%;height: 100%;background-color: #222222;box-shadow: 0 0 50px -20px #000;border-radius: 2%;overflow: hidden;">
+      <div id="form_header_container" style="width: 100%;
+      height: 5%;display: flex; justify-content: center;align-items: center;float: left; padding: 20px;padding-bottom: 30px; padding-top: 30px;
+      border-bottom: 1px solid transparent; border-color: cornflowerblue;background: #000;">
+          <h2 id="form_header" style="display: inline-block; font-size: 15px;font-family: Bowlby One SC;
+          font-weight: 900;  text-transform: uppercase;letter-spacing: 1px; background-color: rgb(255, 255, 255);-webkit-background-clip: text;-webkit-text-fill-color: transparent;"> MEMBERS PORTAL </h2>
+      </div>
+
+      <div id="form_content_container" style="width: 100%; height: 90%; float: left; display: flex;justify-content: center;align-items: center;padding-top: 30px;">
+          <div id="form_content_inner_container" style="width: 75%;height: 100%;float: left;">
            
-                    <th scope="row" >
-                       <label>${data.regno}</label>
-                     </th>
-                         <td>${data.name}</td>
-                         <td style="text-align:center;font-weight:800;color: ${data.status}">${data.AAper}</td>
-                         <td>${data.Sept22a}</td>
-                         <td>${data.Sept22a}</td>
-                         <td>${data.Aug22b}</td>
-                        <td>${data.Aug22a}</td>
-                        <td>${data.Jul22b}</td>
-                        <td>${data.Jul22a}</td>
-                         <td>${data.Jun}</td>
-                         <td>${data.May2}</td>
-                         <td>${data.May1}</td>
-                         <td>${data.Apr2}</td>
-                         <td>${data.Apr1}</td>
-                         <td>${data.Mar2}</td>
-                         <td>${data.Mar1}</td>
-                         <td>${data.Feb2}</td>
-                         <td>${data.Feb1}</td>
-                    
-                   </tr>`;
-         let table = document.getElementById('myTable2')
-         table.innerHTML += row
-     })
-    })
+          <form autocomplete="off" onsubmit="login(event)" >
+              
+
+            <input type="email" id="loginEmail" placeholder="Email" style="width: 100%;
+              height: 40px;padding-left: 10px;margin-bottom: 20px;background: #000;font-family: Montserrat;font-weight: 500;color: #fff;
+              font-size: 12px; border-bottom: 2px solid transparent;border-top-left-radius: 2%; border-top-right-radius: 2%;border-color: cornflowerblue;">
+             
+
+              <input type="password" id="loginPassword" placeholder="Password" style="width: 100%;
+              height: 40px;padding-left: 10px;margin-bottom: 20px;background: #000;font-family: Montserrat;font-weight: 500;color: #fff;
+              font-size: 12px; border-bottom: 2px solid transparent;border-top-left-radius: 2%; border-top-right-radius: 2%;border-color: cornflowerblue;">
+              <a class="modal-trigger" href="#modal2">Forgot Password</a><br>
+              <div id="button_container" style="width: 100%;height: 45px;background-color: cornflowerblue;color: #fff;margin-top: 5px;" >
+                  <button type="submit" style="width: 100%;height: 100%;background: transparent;color: inherit;font-family: Montserrat;letter-spacing: 1px;
+                  font-weight: 900;font-size: 12px;cursor: pointer;align-items: center;">Login</button>
+                  
+              </div>
+           </form>
+          </div>
+      </div></div>
+  </div>
+      `
+    
+       
+    }
+
+
+}
+
+
+
+async function getuserInfoRealtime(userID){
+    if(userID){
+      const userdocRef = await  firebase.firestore()
+        .collection('users')
+        .doc(userID)
+        userdocRef.onSnapshot((doc)=>{
+            if(doc.exists){
+                 const userInfo = doc.data()
+                    if(userInfo){
+                        Atten.innerHTML = `
+
+                        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+
     
 
+    <div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title">Main</li>
 
-.catch(err=>{
- console.log(`Error: ${err}`)
-});
+               
+
+                <li>
+                    <a href="main.html"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                </li>
+
+                <li >
+                           <a href="activities.html"><i class="fa fa-bell-o"></i> <span>Notifications</span></a>
+                      </li>
+            
+                               
+               
+                <li>
+                <a href="employees.html"><i class="fa fa-users"></i> <span>Members List</span></a>
+               </li>
+
+               <li>
+                <a href="attendance.html"><i class="fa fa-calendar-check-o"></i> <span>Attendance</span></a>
+               </li>
+
+               <li>
+               <a href="attenUpdates.html"><i class="fa fa-pencil"></i> <span>Attendance Updates</span></a>
+              </li>
+
+               <li>
+               <a href="leaves.html"><i class=" fa fa-edit"></i> <span>Leaves</span></a>
+              </li>
+
+                <li>
+                    <a href="payments.html"><i class="fa fa-money active"></i> <span>Payments</span></a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+
+        
 
 
-
-
-// Batch1 (2nd Year) batch
-db.collection("attendance").orderBy('regno').limit(32).get().then(querySnapshot=>{
-    querySnapshot.forEach(doc=>{
-        let data = doc.data();
-        let row  = `<tr>
+ <div class="page-wrapper">
+            <div class="content">
           
-                   <th scope="row">
-                      <label>${data.regno}</label>
-                    </th>
-                        <td>${data.name}</td>
-                        <td style="text-align:center;font-weight:800;color: ${data.status}">${data.AAper}</td>
-                        <td>${data.Sept22a}</td>
-                        <td>${data.Sept22a}</td>
-                        <td>${data.Aug22b}</td>
-                        <td>${data.Aug22a}</td>
-                        <td>${data.Jul22b}</td>
-                        <td>${data.Jul22a}</td>
-                        <td>${data.Jun}</td>
-                     
+                <div class="row">
+                    <div class="col-lg-12">
+						
+                        
+                        <div class="testbox">
+                        <form onsubmit="updateattendance(event)" id="attendanceform">
+                        <h1 style="color: #fff;text-align: center;">WEEKLY ATTENDANCE</h1>
                        
-                     
-                  </tr>`;
-        let table = document.getElementById('myTable3')
-        table.innerHTML += row
-    })
-   })
-    
+                          <hr>
+                          <p class="container-fluid" style="text-align: center;font-size: 20px;color: rgba(78, 207, 3, 0.979);border: 1px solid  rgba(78, 207, 3, 0.979);padding: 5px;" id="demo"></p>
+                          
+                          <h5 style="color: white;font-weight: 200;font-size:13px;text-align:left">I, <span style="color:#009efb;font-weight:400;font-size:14px">${userInfo.name} [${userInfo.regno}]</span> post my attendance for the <span style="font-weight: 500;">second half of August</span> <span style="font-size: 14px;color: #009efb;">[15/08/2022 - 31/08/2022]</span></h5>
+                          <br><label style="text-align: left">Tasks Updates</label>
+                        <textarea id="work" maxlength="300" style="padding: 1px;color:white;border: 1px solid  rgba(78, 207, 3, 0.979)" required ></textarea>
+                          
+                          <select id="attend" style="color: white;font-weight: 400;">
+                            <option style="color: rgb(5, 216, 33);font-weight: 400;" value="Present">Present</option>
+                            <option style="color: red;font-weight: 400;" value="Absent">Absent</option>
+                          </select>
+                                <div class="alert">Attendance posted successfully</div>
+                          <div class="btn-block">
+                          
+                            <button style="background-color:rgba(78, 207, 3, 0.979); font-size:15px" id="send" type="submit" >Post</button> 
+                             </div>
+                          <div class="container-fluid" style="margin-bottom:20px;">
+           
+            </div>
+                        </form>
 
+                        <button onclick="location.href='attenUpdates.html'" style="border: 1px solid white;background-color:black;color:#fff"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> Updates</button>
+                      </div> 
+                    
+                    
+                    </div>
+            </div>
+           
+        </div>
+    </div>
+         
+        </div>
 
-.catch(err=>{
- console.log(`Error: ${err}`)
-});
-
-
-
-
-
-// Third batch
-db.collection("attendance").orderBy('regno').limitToLast(4).get().then(querySnapshot=>{
-    querySnapshot.forEach(doc=>{
-        let data = doc.data();
-        let row  = `<tr>
-          
-                   <th scope="row">
-                      <label>${data.regno}</label>
-                    </th>
-                        <td>${data.name}</td>
-                        <td style="text-align:center;font-weight:800;color: ${data.status}">${data.AAper}</td>
-                        <td>${data.Sept22a}</td>
-                        <td>${data.Sept22a}</td>
-                        <td>${data.Aug22b}</td>
-                       <td>${data.Aug22a}</td>
-                       <td>${data.Jul22b}</td>
+        
+                       
+                        `
+               
+                
                       
-                   
-                  </tr>`;
-        let table = document.getElementById('myTable4')
-        table.innerHTML += row
-    })
-   })
-   
+
+                }    
+             }
+        })
 
 
-.catch(err=>{
-console.log(`Error: ${err}`)
-});
+    }else{
+        Atten.innerHTML = `<div id="content_container" class="container" style=" width: 47vh;
+        height: 43vh;margin-top:180px">
+        
+        <div id="form_container" style="width: 100%;height: 100%;background-color: #222222;box-shadow: 0 0 50px -20px #000;border-radius: 2%;overflow: hidden;">
+        <div id="form_header_container" style="width: 100%;
+        height: 5%;display: flex; justify-content: center;align-items: center;float: left; padding: 20px;padding-bottom: 30px; padding-top: 30px;
+        border-bottom: 1px solid transparent; border-color: cornflowerblue;background: #000;">
+            <h2 id="form_header" style="display: inline-block; font-size: 15px;font-family: Bowlby One SC;
+            font-weight: 900;  text-transform: uppercase;letter-spacing: 1px; background-color: rgb(255, 255, 255);-webkit-background-clip: text;-webkit-text-fill-color: transparent;"> MEMBERS PORTAL </h2>
+        </div>
+
+        <div id="form_content_container" style="width: 100%; height: 90%; float: left; display: flex;justify-content: center;align-items: center;padding-top: 30px;">
+            <div id="form_content_inner_container" style="width: 75%;height: 100%;float: left;">
+             
+            <form autocomplete="off" onsubmit="login(event)" >
+                
+
+              <input type="email" id="loginEmail" placeholder="Email" style="width: 100%;
+                height: 40px;padding-left: 10px;margin-bottom: 20px;background: #000;font-family: Montserrat;font-weight: 500;color: #fff;
+                font-size: 12px; border-bottom: 2px solid transparent;border-top-left-radius: 2%; border-top-right-radius: 2%;border-color: cornflowerblue;">
+               
+
+                <input type="password" id="loginPassword" placeholder="Password" style="width: 100%;
+                height: 40px;padding-left: 10px;margin-bottom: 20px;background: #000;font-family: Montserrat;font-weight: 500;color: #fff;
+                font-size: 12px; border-bottom: 2px solid transparent;border-top-left-radius: 2%; border-top-right-radius: 2%;border-color: cornflowerblue;">
+                <a class="modal-trigger" href="#modal2">Forgot Password</a><br>
+                
+                <div id="button_container" style="width: 100%;height: 45px;background-color: cornflowerblue;color: #fff;margin-top: 5px;" >
+                    <button type="submit" style="width: 100%;height: 100%;background: transparent;color: inherit;font-family: Montserrat;letter-spacing: 1px;
+                    font-weight: 900;font-size: 12px;cursor: pointer;align-items: center;">Login</button>
+                    
+                </div>
+             </form>
+            </div>
+        </div></div>
+    </div>
+
+    </div></div>
+
+
+    <div class="sidebar-overlay" data-reff=""></div>
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.js"></script>
+    <script src="assets/js/Chart.bundle.js"></script>
+    <script src="assets/js/chart.js"></script>
+    <script src="assets/js/app.js"></script>
+        `
+    }
+}
+
+
+
+
+
+//Attendance
+
+
+function updateattendance(event){
+  event.preventDefault()
+  var Aug22b = document.getElementById('attend').value
+  var Aug22bf = document.getElementById('work').value
+  var userRef = firebase.firestore().collection('attendance').doc(firebase.auth().currentUser.uid);
+
+  var setWithMerge = userRef.set({
+    Aug22b:Aug22b,
+    Aug22bf:Aug22bf
+
+  },{ merge: true}).then(()=>{
+    document.querySelector('.alert').style.display = 'block';
+
+     // Show alert
+ document.querySelector('.alert').style.display = 'block';
+
+ // Hide alert after 3 seconds
+ setTimeout(function(){
+   document.querySelector('.alert').style.display = 'none';
+ },5000);
+
+ // Clear form
+ document.getElementById('attendanceform').reset();
+
+  });
+}
+
+
+
+
+           
+
+     // Set the date we're counting down to
+     var countDownDate = new Date("August 31, 2022 22:00:00").getTime();
+     
+     // Update the count down every 1 second
+     var x = setInterval(function() {
+     
+       // Get today's date and time
+       var now = new Date().getTime();
+     
+       // Find the distance between now and the count down date
+       var distance = countDownDate - now;
+     
+       // Time calculations for days, hours, minutes and seconds
+       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     
+       // Display the result in the element with id="demo"
+       document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+       + minutes + "m " + seconds + "s ";
+
+
+ 
+     
+       // If the count down is finished, write some text
+       if (distance < 0) {
+         clearInterval(x);
+         document.getElementById("demo").innerHTML = "Attendance closed for the week";
+         document.getElementById("demo").style.color = "red";
+         document.getElementById("demo").style.borderColor = "red";
+         document.getElementById("work").style.borderColor = "red";
+         document.querySelector('#work').disabled = "disabled";
+         document.querySelector('#send').style.color = "white";
+         document.querySelector('#send').style.background = "red";
+         document.querySelector('#send').disabled = "disabled";
+      
+       }
+     }, 1000);
+    
