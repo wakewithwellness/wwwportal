693a36e20cdfc2388715409304cccf1f225e830e
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-09-04 23:06:05 +0530

    Update extraform.js

--- MembersPanel/scripts/extraform.js
+++ MembersPanel/scripts/extraform.js
@@ -88,3 +88,54 @@ function saveData(Membername, Email, Contact, Whatsapp, Alternate, College, Depa
 
   });
 }
+
+
+
+
+
+
+           
+
+     // Set the date we're counting down to
+     var countDownDate = new Date("September 5, 2022 18:01:00").getTime();
+     
+     // Update the count down every 1 second
+     var x = setInterval(function() {
+     
+       // Get today's date and time
+       var now = new Date().getTime();
+     
+       // Find the distance between now and the count down date
+       var distance = countDownDate - now;
+     
+       // Time calculations for days, hours, minutes and seconds
+       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     
+       // Display the result in the element with id="demo"
+       document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+       + minutes + "m " + seconds + "s ";
+
+
+ 
+     
+       // If the count down is finished, write some text
+       if (distance < 0) {
+         clearInterval(x);
+         document.getElementById("demo").innerHTML = "Submissions closed";
+         document.getElementById("demo").style.color = "red";
+         document.getElementById("demo").style.borderColor = "red";
+
+         document.querySelector('#submit').style.color = "white";
+         document.querySelector('#submit').style.background = "#cccccc";
+         document.querySelector('#submit').style.border = "none";
+         document.querySelector('#submit').disabled = "disabled";
+         document.querySelector('#submit').style.cursor = "not-allowed";
+
+
+      
+       }
+     }, 1000);
+    
