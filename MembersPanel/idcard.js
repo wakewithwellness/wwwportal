1c5f1cbc52678f7284d54834fbbefb14aa29b80e
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-09-20 16:16:56 +0530

    Update idcard.js

--- MembersPanel/idcard.js
+++ MembersPanel/idcard.js
@@ -1,4 +1,4 @@
- // Reference messages collection
+  // Reference messages collection
          var dataRef = firebase.database().ref('IDreg');
 
 // Listen for form submit
@@ -6,6 +6,8 @@ document.getElementById('idreg').addEventListener('submit', submitForm);
 
 // Submit form
 function submitForm(e){
+  var rzp1 = new Razorpay(options);
+  rzp1.open();
   e.preventDefault();
 
   // Get values
@@ -13,6 +15,7 @@ function submitForm(e){
   var Email = getInputVal('email');
   var Contact = getInputVal('contact');
   var Blood = getInputVal('blood');
+  var Amount = getInputVal('amount');
   var Address = getInputVal('address');
 
 
@@ -20,9 +23,14 @@ function submitForm(e){
 
 
   // Save message
-  saveData(name, Email, Contact, Blood, Address);
+  saveData(name, Email, Contact, Blood,Amount, Address);
+
+
+  document.getElementById('idreg').reset();
 
 }
+
+
 
 // Function to get get form values
 function getInputVal(id){
@@ -30,18 +38,95 @@ function getInputVal(id){
 }
 
 // Save message to firebase
-function saveData(name, Email, Contact, Blood, Address){
+function saveData(name, Email, Contact, Blood,Amount, Address){
   var newDataRef = dataRef.push();
   newDataRef.set({
     name: name,
     Email:Email,
     Contact:Contact,
     Blood:Blood,
+    Amount:Amount,
     Address:Address
   
     
 
   });
 }
+
+
+
+
+  var Email = getInputVal('email');
+  var Contact = getInputVal('contact');
+var Amount = getInputVal('amount');
+var options = {
+ 
+  "key": "rzp_live_HtAyNsIrtPOAM7", 
+ 
+  "key_secret":"mZwUmUGtPJEVqpqrE36uVFYa",
+   amount: Amount *100,
+   name:"Wake With Wellness",
+   description:"ID Card fee",
+   close: false,
+   callback_url: 'https://your-server/callback_url',
+   redirect: true,
+  "prefill":
+  {
+
+        email:Email,
+        contact: Contact
+  },
+
+
+  config: {
+    display: {
+      blocks: {
+        banks: {
+          name: 'All payment methods',
+          instruments: [
+            {
+              method: 'upi'
+            },
+            {
+              method: 'card'
+            },
+            {
+                method: 'wallet'
+            },
+            {
+                method: 'netbanking'
+            },
+            {
+                method: 'emi'
+            }
+          ],
+        },
+      },
+      sequence: ['block.banks'],
+      preferences: {
+        show_default_blocks: false,
+      },
+    },
+  },
+
+
+
+
+
+  "handler": function (response) {
+    alert(response.razorpay_payment_id);
+  },
+  "modal": {
+    "ondismiss": function () {
+      if (confirm("Are you sure, you want to close the form?")) {
+        txt = "You pressed OK!";
+        console.log("Checkout form closed by the user");
+      } else {
+        txt = "You pressed Cancel!";
+        console.log("Complete the Payment")
+      }
+    }
+  }
+};
 
 
