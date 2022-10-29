aca95db3198d1612fe231b84915f5cb214b13fe9
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-10-29 14:13:33 +0530

    Update firestore-db.js

--- Board/scripts/firestore-db.js
+++ Board/scripts/firestore-db.js
@@ -50,18 +50,13 @@ async function getuserInfo(userID){
       <form autocomplete="off" onsubmit="login(event)">
           <input type="email" class="form-control" id="loginEmail" placeholder="Email" style="width: 98%;
           height: 40px;padding-left:8px;font-size: 13px;border: 1px solid #e8e8e8; border-radius:5px">
-
           <input type="password" class="form-control" id="loginPassword" placeholder="Password" style="width: 98%;
           height: 40px;padding-left:8px;font-size: 13px;border: 1px solid #e8e8e8; border-radius:5px">
-
           <br>
           <button type="submit" class="btn" style="background-color:#0d6efd;color: #fff; padding-bottom:38px; float: left"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</button>
           
   </form>
         <div>
-
-
-
        
       </div>
     </div></div>
@@ -85,67 +80,46 @@ async function getuserInfoRealtime(userID){
                  const userInfo = doc.data()
                     if(userInfo){
                         userDetails.innerHTML = `
-
                         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-
-
     
-
                         <div class="sidebar" id="sidebar">
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
                         <li class="menu-title" style="text-align: left">Boards Panel</li>
-
                        
-
                     <li >
                     <a href="attenUpdates.html"><i class="fa fa-calendar"></i> <span>Attendance</span></a>
                     </li>
-
-
                     <li>
                     <a href="members.html"><i  class="fa fa-users"></i> <span>Members</span></a>
                    </li>
-
   
-
                        <li>
                        <a href="leavepanel.html"><i class=" fa fa-edit"></i> <span>Leaves</span></a>
                       </li>
-
                       <li>
                       <a href="payments.html"><i  class="fa fa-money"></i> <span>Payments</span></a>
                      </li>
-
                      <li>
                      <a href="recruitments.html"><i  class="fa fa-user"></i> <span>Registrations</span></a>
                     </li>
           
-
                        
-
                     </ul>
                 </div>
             </div>
         </div>
-
-
-
-
-
         
         <div class="page-wrapper">
-
         <div style="float: right; margin-right:15px">
         <a type="button" style="background-color:red;padding:8.5px;color:white;margin-left: 5px; border-radius: 4px; cursor: pointer" data-toggle="modal" data-target="#staticBackdrop"> <i class="fa fa-sign-out"></i> SignOut</a>
         
        
       </div>
-
         <div class="content" style="background-color: #fff;margin: 15px;border-top: 3px solid #0d6efd;padding: 20px;border-radius: 5px">
         
         <div class="row">
@@ -154,8 +128,6 @@ async function getuserInfoRealtime(userID){
                <hr>
            </div>
            </div>
-
-
            <div class="row">
            <div class="col-sm-2"> 
           
@@ -163,7 +135,6 @@ async function getuserInfoRealtime(userID){
                    <div class="profile-img-wrap" style="height:25vh;width:24vh" >
                        <div class="profile-img">
                            <img id="proimg" class="pic modal-trigger" style="border-radius: 10px" href="#modal4" src="../assets/user.png" alt=""><br>
-
                            <i href="#modal4" class="fa fa-upload modal-trigger" style="margin-top: 10px"> Upload</i>
                        </div>
                        
@@ -173,63 +144,37 @@ async function getuserInfoRealtime(userID){
            </div>
            <div class="col-sm"> 
            <div class="row" style="text-align:left">
-
-
            <div class="col-sm">
            <h4 style="font-size: 13px">Name: <span style="color: #0d6efd">${userInfo.name}</span></h4>
            </div>
-
            <div class="col-sm">
            <h4 style="font-size: 13px">Registration no.: <span style="color: #0d6efd">${userInfo.regno}</span></h4>
            </div>
-
            <div class="col-sm">
            <h4 style="font-size: 13px">E-mail: <span style="color: #0d6efd">${userInfo.email}</span></h4>
            </div>
-
          
-
            </div>
-
-
            <div class="row" style="text-align:left">
-
            <div class="col-sm">
            <h4 style="font-size: 13px">Contact no.: <span style="color: #0d6efd">${userInfo.phone}</span></h4>
            </div>
-
            <div class="col-sm">
            <h4 style="font-size: 13px">Whatsapp no.: <span style="color: #0d6efd">${userInfo.whatsapp}</span></h4>
            </div>
-
            <div class="col-sm">
            <h4 style="font-size: 13px">Department: <span style="color: #0d6efd">${userInfo.department}</span></h4>
            </div>
-
            </div>
-
-
            <div class="row" style="text-align:left">
            <div class="col-sm">
            <h4 style="font-size: 13px">Address: <span style="color: #0d6efd">${userInfo.address}</span></h4>
            </div>
-
-           </div>
-
            </div>
            </div>
-
+           </div>
 </div>
-
-
-
-
-
-
 <div class="content" style="background-color: #fff;margin: 15px;border-top: 3px solid #0d6efd;border-radius: 5px"">
-
-
-
 <div class="row">
 <div class="col-lg">
 <h4 class="page-title" style="text-align:left">Members Panel</h4>
@@ -243,18 +188,15 @@ async function getuserInfoRealtime(userID){
 													
 													<h2><a href="attenUpdates.html"  style="color: green;font-weight: 500;">Attendance</a></h2>
 												</td>                 
-
 												<td class="text-right">
 													<a href="attenUpdates.html"  class="btn btn-outline-success take-btn">View</a>
 												</td>
 											</tr>
-
                                             <tr>
 												<td style="min-width: 200px;">
 													
 													<h2><a href="./Meeting/meeting.html"  style="color: red;font-weight: 500;">Meeting Attendance</a></h2>
 												</td>                 
-
 												<td class="text-right">
 													<a href="./Meeting/meeting.html"  class="btn btn-outline-danger take-btn">View</a>
 												</td>
@@ -265,7 +207,6 @@ async function getuserInfoRealtime(userID){
                                                 
                                                 <h2><a href="./filesUpload.html"  style="color: blue;font-weight: 500;">Contents Uploaded</a></h2>
                                             </td>                 
-
                                             <td class="text-right">
                                                 <a href="./filesUpload.html"  class="btn btn-outline-primary take-btn">View</a>
                                             </td>
@@ -273,40 +214,44 @@ async function getuserInfoRealtime(userID){
 
 
 
+                                        <tr>
+                                        <td style="min-width: 200px;">
+                                            
+                                            <h2><a href="./OfflineEventsAtten.html"  style="color: green;font-weight: 500;">Offline Events Attendance</a></h2>
+                                        </td>                 
+                                        <td class="text-right">
+                                            <a href="./OfflineEventsAtten.html"  class="btn btn-outline-success take-btn">View</a>
+                                        </td>
+                                    </tr>
+
+
 											<tr>
 												<td style="min-width: 200px;">
 												
-													<h2><a href="recruitments.html">Registrations</a></h2>
+													<h2><a href="recruitments.html">Recruitments</a></h2>
 												</td>                 
-
 												<td class="text-right">
 													<a href="recruitments.html" class="btn btn-outline-primary take-btn">View</a>
 												</td>
 											</tr>
-
                                             <tr>
                                             <td style="min-width: 200px;">
                                                 
                                                 <h2><a href="members.html">Members List</a></h2>
                                             </td>                 
-
                                             <td class="text-right">
                                                 <a href="members.html" class="btn btn-outline-primary take-btn">View</a>
                                             </td>
                                         </tr>
-
-
 											<tr>
 												<td style="min-width: 200px;">
 												
 													<h2><a href="agreementpanel.html">Agreements</a></h2>
 												</td>                 
-
 												<td class="text-right">
 													<a href="agreementpanel.html" class="btn btn-outline-primary take-btn">View</a>
 												</td>
 											</tr>
-
                                         <tr>
                                             <td style="min-width: 200px;">    
                                                 <h2><a href="payments.html">Payments</a></h2>
@@ -315,8 +260,6 @@ async function getuserInfoRealtime(userID){
                                                 <a href="payments.html" class="btn btn-outline-primary take-btn">View</a>
                                             </td>
                                         </tr>
-
-
                                         <tr>
 											<td style="min-width: 200px;">	
 												<h2><a href="membersfeedback.html">Member's Feedbacks</a></h2>
@@ -325,7 +268,6 @@ async function getuserInfoRealtime(userID){
 												<a href="membersfeedback.html" class="btn btn-outline-danger take-btn">View</a>
 											</td>
 										</tr>
-
 				
 										<tr>
 											<td style="min-width: 200px;">
@@ -335,8 +277,6 @@ async function getuserInfoRealtime(userID){
 												<a href="leavepanel.html" class="btn btn-outline-primary take-btn">View</a>
 											</td>
 										</tr>
-
-
                                         <tr>
                                             <td style="min-width: 200px;"> 
                                                 <h2><a href="../MembersPanel/agreement.html">Member Agreement Form</a></h2>
@@ -345,8 +285,6 @@ async function getuserInfoRealtime(userID){
                                                 <a href="../MembersPanel/agreement.html" class="btn btn-outline-primary take-btn">View</a>
                                             </td>
                                         </tr>
-
-
                                         <tr>
                                             <td style="min-width: 200px;">
                                                 <h2><a href="detailUpdates.html">Attendance Details</a></h2>
@@ -355,19 +293,11 @@ async function getuserInfoRealtime(userID){
                                                 <a href="detailUpdates.html" class="btn btn-outline-primary take-btn">View</a>
                                             </td>
                                         </tr>
-
 										</tbody>
 						</table>
 			</div>
             <hr>
      </div>
-
-
-
-
-
-
-
     <div class="col-lg">
     <h4 class="page-title" style="text-align:left">Data Panel</h4>
 <hr>
@@ -375,94 +305,68 @@ async function getuserInfoRealtime(userID){
             <table class="table mb-0">
             
                 <tbody>
-
                 <tr>
                 <td style="min-width: 200px;">
                     
                     <h2><a href="bloodneeded.html"  >Blood Request</a></h2>
                 </td>                 
-
                 <td class="text-right">
                     <a href="bloodneeded.html"  class="btn btn-danger take-btn">View</a>
                 </td>
             </tr>
-
-
-
                     <tr>
                     <td style="min-width: 200px;">
                         
                         <h2><a href="BloodCamp.html"  >Blood Camp Data</a></h2>
                     </td>                 
-
                     <td class="text-right">
                         <a href="BloodCamp.html"  class="btn btn-primary take-btn">View</a>
                     </td>
                 </tr>
-
                 <tr>
                 <td style="min-width: 200px;">
                     
                     <h2><a href="childrensday.html">Children's Day Funds</a></h2>
                 </td>                 
-
                 <td class="text-right">
                     <a href="childrensday.html"  class="btn btn-success take-btn">View</a>
                 </td>
             </tr>
-
-
                 <tr>
                         <td style="min-width: 200px;">
                             
                             <h2><a href="volunteershipadmin.html"  >Volunteership Registration</a></h2>
                         </td>                 
-
                         <td class="text-right">
                             <a href="volunteershipadmin.html"  class="btn btn-outline-primary take-btn">View</a>
                         </td>
                     </tr>
-
-
-
-
                    
-
                 
                 <tr>
                 <td style="min-width: 200px;">
                     
                     <h2><a href="wellnessfund.html"  >Wellness Fundraiser</a></h2>
                 </td>                 
-
                 <td class="text-right">
                     <a href="wellnessfund.html"  class="btn btn-outline-primary take-btn">View</a>
                 </td>
             </tr>
-
-
-
             <tr>
             <td style="min-width: 200px;">
                 
                 <h2><a href="finance.html"  >Finance Updates</a></h2>
             </td>                 
-
             <td class="text-right">
                 <a href="finance.html"  class="btn btn-outline-primary take-btn">View</a>
             </td>
         </tr>
-
-
-
                 </tbody>
             </table>
         </div>
-
 <hr>
         <h4 class="page-title" style="text-align:left;">Coordinator's Panel</h4>
         <hr>
-
         <div class="table-responsive" style="overflow-x: hidden">
 									<table class="table mb-0">
 									
@@ -493,26 +397,14 @@ async function getuserInfoRealtime(userID){
                                     </tr>
                         
 											
-
                                                       
 										</tbody>
 									</table>
 								</div>
-
-
-
     </div>
-
 </div>
                       
 </div>
-
-
-
-
-
-
-
             <div class="content">
                 <div class="row">
                     <div class="col-md-6 col-sm-6 col-lg-6 col-xl-4">
@@ -560,7 +452,6 @@ async function getuserInfoRealtime(userID){
                              </div>
                          </div>
                      </div>
-
                      <div class="col-md-6 col-sm-6 col-lg-6 col-xl-4">
                          <div class="dash-widget" style="border-top: 3px solid #0d6efd;border-radius: 5px">
                                     <span class="dash-widget-bg1"><i class="fa fa-user-o" aria-hidden="true"></i></span>
@@ -573,19 +464,14 @@ async function getuserInfoRealtime(userID){
                 </div>
 				
                 </div>
-
                 
-
 <div class="content" style="background-color: #fff;margin: 15px;border-top: 3px solid #0d6efd;border-radius: 5px">
-
 <div class="row">
 <div class="col-sm-12">
    <h4 class="page-title" style="text-align:left">My Files </h4>
    <hr>
 </div>
 </div>
-
-
 <div style="text-align:left; cursor:pointer">
 <h5 style="font-size: 13px" onclick="location.href='${userInfo.agreement}'">Agreement letter <a style="color: #0d6efd"><i class="fa fa-hand-o-right" aria-hidden="true"></i> Download</a></h5>
 <h5 style="font-size: 13px" onclick="location.href='${userInfo.certificate}'">Download your certificate <a style="color: #0d6efd"><i class="fa fa-hand-o-right" aria-hidden="true"></i> Download</a></h5>          
@@ -593,42 +479,22 @@ async function getuserInfoRealtime(userID){
 </div>
                       
 </div>
-
-
-
 <div class="content" style="background-color: #fff;margin: 15px;border-top: 3px solid #0d6efd;border-radius: 5px"">
-
 <div class="row">
 <div class="col-sm-12">
    <h4 class="page-title" style="text-align:left">Code of Ethics and Professional Conduct</h4>
    <hr>
 </div>
 </div>
-
-
 <div style="text-align:left; cursor:pointer">
-
 <h4 style="font-size: 13px" class="modal-trigger" href="#exampleModal">Code of Ethics and Professional Conduct <a style="color: #0d6efd"><i class="fa fa-hand-o-right" aria-hidden="true"></i> Download</a></h4>
 </div>
                       
 </div>
-
-
-
 <br><br>
-
-
-
-
            
    
-
-
-
     
-
-
-
                        
                         `
                         editProfile["name"].value = userInfo.name
@@ -676,8 +542,6 @@ async function getuserInfoRealtime(userID){
          
         </div>
       </div></div>
-
-
     <div class="sidebar-overlay" data-reff=""></div>
     <script src="../MembersPanel/assets/js/jquery-3.2.1.min.js"></script>
 	<script src="../MembersPanel/assets/js/popper.min.js"></script>
