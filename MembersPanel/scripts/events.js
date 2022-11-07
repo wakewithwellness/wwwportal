5216d1a1f941ff4d5709917c4a32557f24152496
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-11-07 22:32:11 +0530

    Update events.js

--- MembersPanel/scripts/events.js
+++ MembersPanel/scripts/events.js
@@ -167,9 +167,9 @@ async function getuserInfoRealtime(userID){
                                       
                                 
                                 <tr>
-                                <td><a>6</a></td>
+                                <td><a>7</a></td>
                                 <td><h2><a>Hepah: From welfare to well-being</a></h2></td>
-                                <td>14th November 2022</td>
+                                <td>13th November 2022</td>
                                 <td>Anand Ashram Kahilipara</td>
                                 <td style="text-align: center;font-weight: 500;color: red"><span style="color: ${userInfo.color5}">${userInfo.event5}</span></td>
                                 <td style="text-align: center"><a href="${userInfo.e5}" style="display:${userInfo.e5}"><i class="fa fa-download" aria-hidden="true"></i></a></td>
@@ -201,7 +201,7 @@ async function getuserInfoRealtime(userID){
                                         
                                      <tr>
                                    
-                                     <td><a>5</a></td>
+                                     <td><a>6</a></td>
                                      <td><h2><a>Swachh Prayaakh - Cleanliness Drive 2.0</a></h2></td>
                                      <td>6th November 2022</td>
                                      <td>Lachit Ghat, Guwahati</td>
