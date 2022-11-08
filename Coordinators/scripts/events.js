6923a3716bfe2f7d38b8cbfcaaa0de2ea61fbaa8
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-11-08 14:52:29 +0530

    Update events.js

--- Coordinators/scripts/events.js
+++ Coordinators/scripts/events.js
@@ -195,7 +195,7 @@ async function getuserInfoRealtime(userID){
                                 <td><a>7</a></td>
                                 <td><h2><a>Hepah: From welfare to well-being</a></h2></td>
                                 <td>13th November 2022</td>
-                                <td>Anand Ashram Kahilipara</td>
+                                <td>Brahmaputra Children Home, Garchuk,Katabari,Ghy</td>
                                 <td style="text-align: center;font-weight: 500;color: red"><span style="color: ${userInfo.color3}">${userInfo.event3}</span></td>
                                 <td style="text-align: center"><a href="${userInfo.e3}" style="display:${userInfo.e3}"><i class="fa fa-download" aria-hidden="true"></i></a></td>
                                 <td  style="text-align: center">
