4134a8c495134f4bf7b82fe19c3c6fffa392a256
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-11-30 22:28:51 +0530

    Update attendance.js

--- MembersPanel/scripts/attendance.js
+++ MembersPanel/scripts/attendance.js
@@ -356,7 +356,7 @@ function updateattendance(event){
            
 
      // Set the date we're counting down to
-     var countDownDate = new Date("November 30, 2022 22:00:00").getTime();
+     var countDownDate = new Date("December 1, 2022 22:00:00").getTime();
      
      // Update the count down every 1 second
      var x = setInterval(function() {
