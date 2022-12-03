077fc2eb4dbb9c9183cbe13a5fb8ae3a42243766
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-12-03 12:41:53 +0530

    Update neconvene.js

--- neconvene.js
+++ neconvene.js
@@ -72,7 +72,7 @@ function saveData(Membername, contact){
            
 
      // Set the date we're counting down to
-     var countDownDate = new Date("December 1, 2022 23:00:00").getTime();
+     var countDownDate = new Date("December 10, 2022 23:00:00").getTime();
      
      // Update the count down every 1 second
      var x = setInterval(function() {
