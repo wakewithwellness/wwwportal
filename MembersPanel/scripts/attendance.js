84d9d2d354e9bd779f06460f62ff6e0cecd0a701
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2022-12-23 09:41:18 +0530

    Update attendance.js

--- MembersPanel/scripts/attendance.js
+++ MembersPanel/scripts/attendance.js
@@ -209,7 +209,7 @@ async function getuserInfoRealtime(userID){
 						
                     <h5 style="color: grey;font-weight: 200;font-size:13px;text-align:left">I, <span style="font-weight: 500;font-size:14px">${userInfo.name} [${userInfo.regno}]</span> post my attendance 
                     from 
-                    <span style="font-size: 14px;color: #198754;font-weight:600" id="date">01/12/2022 - 15/12/2022</span></h5>
+                    <span style="font-size: 14px;color: #198754;font-weight:600" id="date">15/12/2022 - 31/12/2022</span></h5>
                               <div class="testbox" >
                           
                                               <form onsubmit="updateattendance(event)" id="attendanceform">
@@ -325,13 +325,13 @@ async function getuserInfoRealtime(userID){
 
 function updateattendance(event){
   event.preventDefault()
-  var Dec22a = document.getElementById('attend').value
-  var Dec22af = document.getElementById('work').value
+  var Dec22b = document.getElementById('attend').value
+  var Dec22bf = document.getElementById('work').value
   var userRef = firebase.firestore().collection('attendance').doc(firebase.auth().currentUser.uid);
 
   var setWithMerge = userRef.set({
-    Dec22a:Dec22a,
-    Dec22af:Dec22af
+    Dec22b:Dec22b,
+    Dec22bf:Dec22bf
 
   },{ merge: true}).then(()=>{
    
@@ -356,7 +356,7 @@ function updateattendance(event){
            
 
      // Set the date we're counting down to
-     var countDownDate = new Date("December 15, 2022 22:00:00").getTime();
+     var countDownDate = new Date("December 31, 2022 22:00:00").getTime();
      
      // Update the count down every 1 second
      var x = setInterval(function() {
