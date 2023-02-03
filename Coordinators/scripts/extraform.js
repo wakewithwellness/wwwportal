3bb6b49425e1750fe00d459050158576d12b0a2c
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2023-02-03 12:57:46 +0530

    Update extraform.js

--- Coordinators/scripts/extraform.js
+++ Coordinators/scripts/extraform.js
@@ -98,7 +98,7 @@ function saveData(Membername, Email, Contact, Whatsapp,dob, Alternate, College, 
            
 
      // Set the date we're counting down to
-     var countDownDate = new Date("February 3, 2022 21:00:00").getTime();
+     var countDownDate = new Date("February 3, 2023 21:00:00").getTime();
      
      // Update the count down every 1 second
      var x = setInterval(function() {
