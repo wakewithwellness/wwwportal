26c8c5ad8fbc84ad41f29435c59e5463f0739383
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2023-04-12 18:59:08 +0530

    Update extraform.js

--- MembersPanel/scripts/extraform.js
+++ MembersPanel/scripts/extraform.js
@@ -97,7 +97,7 @@ function saveData(Membername, Email, Contact, Whatsapp, Alternate, College, Depa
            
 
      // Set the date we're counting down to
-     var countDownDate = new Date("November 2, 2022 18:01:00").getTime();
+     var countDownDate = new Date("April 13, 2022 23:00:00").getTime();
      
      // Update the count down every 1 second
      var x = setInterval(function() {
