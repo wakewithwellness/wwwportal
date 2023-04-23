756172715d81bb978a2fa61ef28c09a8d6f49403
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2023-04-23 22:13:56 +0530

    Update firestore-db.js

--- MembersPanel/scripts/firestore-db.js
+++ MembersPanel/scripts/firestore-db.js
@@ -1,53 +1,49 @@
-const userDetails  = document.querySelector('.userDetails')
-const editProfile  = document.querySelector('#editProfile')
+const userDetails = document.querySelector(".userDetails");
+const editProfile = document.querySelector("#editProfile");
 
-
-function createUserCollection(user){
-   firebase.firestore().collection('users')
-   .doc(user.uid)
-   .set({
-       uid:user.uid,
-       name:user.displayName,
-       email:user.email,
-       regno:"",
-       phone:"",
-       whatsapp:"",
-       department:"",
-       department2:"",
-       college:"",
-       status:"",
-       blood:"",
-       gender:"",
-       address:"",
-       state:"",
-       pincode:"",
-       exp:"",
-       agreement: "",
-       idcard:"",
-       certificate:"",
-       status:""
-
-   })
+function createUserCollection(user) {
+  firebase.firestore().collection("members").doc(user.uid).set({
+    uid: user.uid,
+    name: user.displayName,
+    email: user.email,
+    regno: "",
+    phone: "",
+    whatsapp: "",
+    department: "",
+    department2: "",
+    college: "",
+    status: "",
+    blood: "",
+    gender: "",
+    address: "",
+    state: "",
+    pincode: "",
+    exp: "",
+    agreement: "",
+    idcard: "",
+    certificate: "",
+    status: "",
+  });
 }
 
+async function getuserInfo(userID) {
+  if (userID) {
+    const userInfoSnap = await firebase
+      .firestore()
+      .collection("members")
+      .doc(userID)
+      .get();
 
-async function getuserInfo(userID){
-    if(userID){
-      const userInfoSnap = await  firebase.firestore()
-    .collection('users')
-    .doc(userID)
-    .get()
-
-   const userInfo = userInfoSnap.data()
-   if(userInfo){
-       userDetails.innerHTML = `
+    const userInfo = userInfoSnap.data();
+    if (userInfo) {
+      userDetails.innerHTML = `
        <h3>${userInfo.name}</h3>
        <h3>${userInfo.email}</h3>
        <h3>${userInfo.phone}</h3>
-       `
-   }    
-    }else{
-      userDetails.innerHTML = ` <div class="container" style="max-width: 80vh;margin-top:180px">
+       `;
+    }
+  } else {
+    userDetails.innerHTML = ` <div class="container" style="max-width: 80vh;margin-top:180px">
       <div class="card" style="border-top: 3px solid #198754">
       <div class="card-body" style="text-align: center">
       <h5 class="card-title" style="text-align: center; color: #198754; font-weight: 700; font-size:30px">MEMBERS PORTAL</h5><hr>
@@ -71,26 +67,21 @@ async function getuserInfo(userID){
        
       </div>
     </div></div>
-      `
-    
-       
-    }
-
-
+      `;
+  }
 }
 
-
-
-async function getuserInfoRealtime(userID){
-    if(userID){
-      const userdocRef = await  firebase.firestore()
-        .collection('users')
-        .doc(userID)
-        userdocRef.onSnapshot((doc)=>{
-            if(doc.exists){
-                 const userInfo = doc.data()
-                    if(userInfo){
-                        userDetails.innerHTML = `
+async function getuserInfoRealtime(userID) {
+  if (userID) {
+    const userdocRef = await firebase
+      .firestore()
+      .collection("members")
+      .doc(userID);
+    userdocRef.onSnapshot((doc) => {
+      if (doc.exists) {
+        const userInfo = doc.data();
+        if (userInfo) {
+          userDetails.innerHTML = `
                         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
@@ -380,34 +371,30 @@ async function getuserInfoRealtime(userID){
 
       
                        
-                        `
-                        editProfile["name"].value = userInfo.name
-                        editProfile["profileEmail"].value = userInfo.email
-                        editProfile["regno"].value = userInfo.regno
-                        editProfile["phoneno"].value = userInfo.phone
-                        editProfile["whatsapp"].value = userInfo.whatsapp
-                        editProfile["blood"].value = userInfo.blood
-                        editProfile["department"].value = userInfo.department
-                        editProfile["department2"].value = userInfo.department2
-                        editProfile["college"].value = userInfo.college
-                        editProfile["address"].value = userInfo.address
-                        editProfile["state"].value = userInfo.state
-                        editProfile["pincode"].value = userInfo.pincode
-                        editProfile["exp"].value = userInfo.exp
+                        `;
+          editProfile["name"].value = userInfo.name;
+          editProfile["profileEmail"].value = userInfo.email;
+          editProfile["regno"].value = userInfo.regno;
+          editProfile["phoneno"].value = userInfo.phone;
+          editProfile["whatsapp"].value = userInfo.whatsapp;
+          editProfile["blood"].value = userInfo.blood;
+          editProfile["department"].value = userInfo.department;
+          editProfile["department2"].value = userInfo.department2;
+          editProfile["college"].value = userInfo.college;
+          editProfile["address"].value = userInfo.address;
+          editProfile["state"].value = userInfo.state;
+          editProfile["pincode"].value = userInfo.pincode;
+          editProfile["exp"].value = userInfo.exp;
 
-
-                        if(firebase.auth().currentUser.photoURL){
-                            document.querySelector('#proimg').src = firebase.auth().currentUser.photoURL
-                        }
-                      
-
-                }    
-             }
-        })
-
-
-    }else{
-        userDetails.innerHTML = `
+          if (firebase.auth().currentUser.photoURL) {
+            document.querySelector("#proimg").src =
+              firebase.auth().currentUser.photoURL;
+          }
+        }
+      }
+    });
+  } else {
+    userDetails.innerHTML = `
         <div class="container-fluid" style="max-width: 80vh;margin-top:180px">
         <div class="card" style="border-top: 3px solid #198754">
         <div class="card-body" style="text-align: center">
@@ -444,121 +431,105 @@ async function getuserInfoRealtime(userID){
     <script src="assets/js/Chart.bundle.js"></script>
     <script src="assets/js/chart.js"></script>
     <script src="assets/js/app.js"></script>
-        `
-    }
+        `;
+  }
 }
 
+function updateUserProfile(e) {
+  e.preventDefault();
+  const userDocRef = firebase
+    .firestore()
+    .collection("users")
+    .doc(firebase.auth().currentUser.uid);
 
-function updateUserProfile(e){
-    e.preventDefault()
-    const userDocRef =  firebase.firestore()
-    .collection('users')
-    .doc(firebase.auth().currentUser.uid)
+  userDocRef.update({
+    name: editProfile["name"].value,
+    email: editProfile["profileEmail"].value,
+    regno: editProfile["regno"].value,
+    phone: editProfile["phoneno"].value,
+    whatsapp: editProfile["whatsapp"].value,
+    blood: editProfile["blood"].value,
+    department: editProfile["department"].value,
+    department2: editProfile["department2"].value,
+    college: editProfile["college"].value,
+    address: editProfile["address"].value,
+    state: editProfile["state"].value,
+    pincode: editProfile["pincode"].value,
+    exp: editProfile["exp"].value,
+  });
+  document.querySelector(".alert").style.display = "block";
 
+  // Show alert
+  document.querySelector(".alert").style.display = "block";
 
-    userDocRef.update({
-        name:editProfile["name"].value,
-        email:editProfile["profileEmail"].value,
-        regno:editProfile["regno"].value,
-        phone:editProfile["phoneno"].value,
-        whatsapp:editProfile["whatsapp"].value,
-        blood:editProfile["blood"].value,
-        department:editProfile["department"].value,
-        department2:editProfile["department2"].value,
-        college:editProfile["college"].value,
-        address:editProfile["address"].value,
-        state:editProfile["state"].value,
-        pincode:editProfile["pincode"].value,
-        exp:editProfile["exp"].value
-
-    })
-    document.querySelector('.alert').style.display = 'block';
-
-    // Show alert
-document.querySelector('.alert').style.display = 'block';
-
-// Hide alert after 3 seconds
-setTimeout(function(){
-  document.querySelector('.alert').style.display = 'none';
-},3000);
-  
+  // Hide alert after 3 seconds
+  setTimeout(function () {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
 }
 
+function uploadImage(e) {
+  console.log(e.target.files[0]);
+  const uid = firebase.auth().currentUser.uid;
+  const fileRef = firebase.storage().ref().child(`/users/${uid}/profile`);
+  const uploadTask = fileRef.put(e.target.files[0]);
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      if (progress == "100")
+        // Show alert
+        document.querySelector(
+          ".success"
+        ).innerHTML = `<i class="fa fa-check-circle" aria-hidden="true"></i> Updated Successfully`;
 
+      // Hide alert after 10 seconds
+      setTimeout(function () {
+        document.querySelector(".success").innerHTML = ``;
+      }, 10000);
 
+      uploader.value = progress;
 
+      console.log("Upload is " + progress + "% done");
+      document.querySelector(".prog").innerHTML = `${progress}%`;
 
-function uploadImage(e){
-    console.log(e.target.files[0])
-    const uid = firebase.auth().currentUser.uid
-    const fileRef = firebase.storage().ref().child(`/users/${uid}/profile`)
-    const uploadTask =  fileRef.put(e.target.files[0])
-    uploadTask.on('state_changed', 
-  (snapshot) => {
-    
-    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    if(progress=='100') 
-     // Show alert
-     document.querySelector('.success').innerHTML=`<i class="fa fa-check-circle" aria-hidden="true"></i> Updated Successfully`;
-              
-     // Hide alert after 10 seconds
-setTimeout(function(){
-document.querySelector('.success').innerHTML=``;
-},10000);
-      
-uploader.value = progress;
-              
-console.log('Upload is ' + progress + '% done');
-document.querySelector('.prog').innerHTML=`${progress}%`;
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log("Upload is paused");
 
-switch (snapshot.state) {
-     case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-       
           break;
-     case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          
-          break;
-}
-}, function (error) {
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log("Upload is running");
 
-// A full list of error codes is available at
-// https://firebase.google.com/docs/storage/web/handle-errors
-switch (error.code) {
-     case 'storage/unauthorized':
+          break;
+      }
+    },
+    function (error) {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case "storage/unauthorized":
           // User doesn't have permission to access the object
           break;
 
-     case 'storage/canceled':
+        case "storage/canceled":
           // User canceled the upload
           break;
 
-     case 'storage/unknown':
+        case "storage/unknown":
           // Unknown error occurred, inspect error.serverResponse
           break;
+      }
+    },
+
+    function () {
+      uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        console.log("File available at", downloadURL);
+        document.querySelector("#proimg").src = downloadURL;
+        firebase.auth().currentUser.updateProfile({
+          photoURL: downloadURL,
+        });
+      });
+    }
+  );
 }
-}, 
-
-function () {
-    
-    uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-      console.log('File available at', downloadURL);
-      document.querySelector('#proimg').src = downloadURL
-      firebase.auth().currentUser.updateProfile({
-        photoURL: downloadURL
-      })
-    });
-  }
-
-
-  
-);
-}
-
-
-
-
-
-
-
