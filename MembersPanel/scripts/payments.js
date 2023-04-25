9d80099e87b95d35729f007504837f36d4ba2db5
WakeWithWellness <84095549+wakewithwellness@users.noreply.github.com>
2023-04-25 15:08:55 +0530

    Update payments.js

--- MembersPanel/scripts/payments.js
+++ MembersPanel/scripts/payments.js
@@ -1,60 +1,60 @@
-const userDetails  = document.querySelector('.userDetails')
-const editProfile  = document.querySelector('#editProfile')
+const userDetails = document.querySelector(".userDetails");
+const editProfile = document.querySelector("#editProfile");
 
+function createUserCollection(user) {
+  firebase.firestore().collection("members").doc(user.uid).set({
+    uid: user.uid,
+    name: user.displayName,
+    email: user.email,
+    department: "",
+    phone: "",
+    i1: "",
+    i2: "",
+    i3: "",
+    i4: "",
+    p1: "",
+    p2: "",
+    p3: "",
+    p4: "",
+    a1: "",
+    a2: "",
+    a3: "",
+    a4: "",
+    d1: "",
+    d2: "",
+    d3: "",
+    d4: "",
+    s1: "",
 
-function createUserCollection(user){
-   firebase.firestore().collection('users')
-   .doc(user.uid)
-   .set({
-       uid:user.uid,
-       name:user.displayName,
-       email:user.email,
-       department:"",
-       phone:"",
-       i1:"",
-       i2:"",
-       i3:"",
-       i4:"",
-       p1:"",
-       p2:"",
-       p3:"",
-       p4:"",
-       a1:"",
-       a2:"",
-       a3:"",
-       a4:"",
-       d1:"",
-       d2:"",
-       d3:"",
-       d4:"",
-       hide:""
-
-       
-
-   })
+    s2: "",
+    s3: "",
+    s4: "",
+    s5: "",
+    hide: "",
+  });
 }
 
+async function getuserInfo(userID) {
+  if (userID) {
+    const userInfoSnap = await firebase
+      .firestore()
+      .collection("members")
+      .doc(userID)
+      .get();
 
-async function getuserInfo(userID){
-    if(userID){
-      const userInfoSnap = await  firebase.firestore()
-    .collection('users')
-    .doc(userID)
-    .get()
-
-   const userInfo = userInfoSnap.data()
-   if(userInfo){
-       userDetails.innerHTML = `
+    const userInfo = userInfoSnap.data();
+    if (userInfo) {
+      userDetails.innerHTML = `
        <h3>${userInfo.name}</h3>
        <h3>${userInfo.email}</h3>
        <h3>${userInfo.phone}</h3>
-       `
-   }    
-    }else{
-      userDetails.innerHTML = ` <div class="container-fluid" style="max-width: 80vh;margin-top:180px">
-      <div class="card" style="border-top: 3px solid #198754">
+       `;
+    }
+  } else {
+    userDetails.innerHTML = ` <div class="container-fluid" style="max-width: 80vh;margin-top:180px">
+      <div class="card" style="border-top: 3px solid rgb(53, 53, 53)">
       <div class="card-body" style="text-align: center">
-      <h5 class="card-title" style="text-align: center; color: #198754; font-weight: 700; font-size:25px">MEMBERS PORTAL</h5><hr>
+      <h5 class="card-title" style="text-align: center; color: rgb(53, 53, 53); font-weight: 700; font-size:25px">MEMBERS PORTAL</h5><hr>
 <br>
       <form autocomplete="off" onsubmit="login(event)">
           <input type="email" class="form-control" id="loginEmail" placeholder="Email" style="width: 98%;
@@ -63,9 +63,9 @@ async function getuserInfo(userID){
           <input type="password" class="form-control" id="loginPassword" placeholder="Password" style="width: 98%;margin-top:10px;
           height: 40px;padding-left:8px;font-size: 13px;border: 1px solid #e8e8e8; border-radius:5px">
           <br>
-          <div style="text-align:left"> <a style="text-decoration: none; color: #198754; font-size: 12px; margin-left: 10px" class="modal-trigger" href="#modal2">Forgot Password</a></div>
+          <div style="text-align:left"> <a style="text-decoration: none; color: rgb(53, 53, 53); font-size: 12px; margin-left: 10px" class="modal-trigger" href="#modal2">Forgot Password</a></div>
           <br> 
-          <button type="submit" class="btn" style="background-color:#198754;color: #fff; padding-bottom:8px; float: left"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</button>
+          <button type="submit" class="btn" style="background-color:rgb(53, 53, 53);color: #fff; padding-bottom:8px; float: left"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</button>
           
   </form>
         <div>
@@ -75,26 +75,21 @@ async function getuserInfo(userID){
        
       </div>
     </div></div>
-      `
-    
-       
-    }
-
-
+      `;
+  }
 }
 
-
-
-async function getuserInfoRealtime(userID){
-    if(userID){
-      const userdocRef = await  firebase.firestore()
-        .collection('Payments')
-        .doc(userID)
-        userdocRef.onSnapshot((doc)=>{
-            if(doc.exists){
-                 const userInfo = doc.data()
-                    if(userInfo){
-                        userDetails.innerHTML = `
+async function getuserInfoRealtime(userID) {
+  if (userID) {
+    const userdocRef = await firebase
+      .firestore()
+      .collection("members")
+      .doc(userID);
+    userdocRef.onSnapshot((doc) => {
+      if (doc.exists) {
+        const userInfo = doc.data();
+        if (userInfo) {
+          userDetails.innerHTML = `
 
                        
 
@@ -151,7 +146,7 @@ async function getuserInfoRealtime(userID){
 
 
 
-<div class="content" style="background-color: #fff;margin: 15px;border-top: 3px solid #198754;padding: 20px;border-radius:5px">
+<div class="content" style="background-color: #fff;margin: 15px;border-top: 3px solid rgb(53, 53, 53);padding: 20px;border-radius:5px">
 <div class="row">
 <div class="col-sm-12">
     <h4 class="page-title">Member Details</h4><hr>
@@ -160,7 +155,7 @@ async function getuserInfoRealtime(userID){
 <div class="row">
 
 <div class="col-lg">
-<h5>Name: <span style="color: #198754">${userInfo.name}</span></h5>
+<h5>Name: <span style="color: rgb(53, 53, 53)">${userInfo.name}</span></h5>
 </div>
 
 
@@ -168,7 +163,7 @@ async function getuserInfoRealtime(userID){
 
 
 <div class="col-lg">
-<h5>Registration no.: <span style="color: #198754">${userInfo.regno}</span></h5>
+<h5>Registration no.: <span style="color: rgb(53, 53, 53)">${userInfo.regno}</span></h5>
 </div>
 
 <div class="col-lg">
@@ -179,7 +174,7 @@ async function getuserInfoRealtime(userID){
 
 
 </div>
-            <div class="content" style="background-color: #fff;margin: 15px;border-top: 3px solid #198754;border-radius:5px">
+            <div class="content" style="background-color: #fff;margin: 15px;border-top: 3px solid rgb(53, 53, 53);border-radius:5px">
            
           
                 <div class="row">
@@ -189,7 +184,7 @@ async function getuserInfoRealtime(userID){
                 </div>
                 <div class="row">
                    
-                        <div class="table-responsive" ${userInfo.i1}>
+                        <div class="table-responsive" ${userInfo.s1}>
                             <table class="table table-bordered">
                                 <thead style="background-color: #f7f7f7">
                                     <tr>
@@ -205,7 +200,7 @@ async function getuserInfoRealtime(userID){
                                 </thead>
                                 <tbody>
                                
-                                    <tr display: ${userInfo.i1}>
+                                    <tr ${userInfo.s1}>
                                         <td><a>${userInfo.i1}</a></td>
                                         <td>
                                             <h2><a>${userInfo.p1}</a></h2>
@@ -216,7 +211,7 @@ async function getuserInfoRealtime(userID){
                                         <td></td>
                                         <td>₹ .00</td>
                                     </tr>
-                                    <tr display: ${userInfo.i2}>
+                                    <tr ${userInfo.s2}>
                                         <td><a>${userInfo.i2}</a></td>
                                         <td>
                                             <h2><a>${userInfo.p2}</a></h2>
@@ -227,7 +222,7 @@ async function getuserInfoRealtime(userID){
                                         <td></td>
                                         <td>₹ .00</td>
                                     </tr>
-                                    <tr display: ${userInfo.i3}>
+                                    <tr ${userInfo.s3}>
                                         <td><a>${userInfo.i3}</a></td>
                                         <td>
                                             <h2><a>${userInfo.p3}</a></h2>
@@ -240,7 +235,7 @@ async function getuserInfoRealtime(userID){
                                     </tr>
 
 
-                                    <tr display: ${userInfo.i4}>
+                                    <tr ${userInfo.s4}>
                                     <td><a>${userInfo.i4}</a></td>
                                     <td>
                                         <h2><a>${userInfo.p4}</a></h2>
@@ -253,7 +248,7 @@ async function getuserInfoRealtime(userID){
                                 </tr>
 
 
-                                <tr display: ${userInfo.i5}>
+                                <tr ${userInfo.s5}>
                                     <td><a>${userInfo.i5}</a></td>
                                     <td>
                                         <h2><a>${userInfo.p5}</a></h2>
@@ -264,6 +259,30 @@ async function getuserInfoRealtime(userID){
                                     <td></td>
                                     <td>₹ .00</td>
                                 </tr>
+
+                                <tr ${userInfo.s6}>
+                                <td><a>${userInfo.i6}</a></td>
+                                <td>
+                                    <h2><a>${userInfo.p6}</a></h2>
+                                </td>
+                            
+                                <td>${userInfo.d6}</td>
+                                <td>₹ ${userInfo.a6}.00</td>
+                                <td></td>
+                                <td>₹ .00</td>
+                            </tr>
+
+                            <tr ${userInfo.s7}>
+                            <td><a>${userInfo.i7}</a></td>
+                            <td>
+                                <h2><a>${userInfo.p7}</a></h2>
+                            </td>
+                        
+                            <td>${userInfo.d7}</td>
+                            <td>₹ ${userInfo.a7}.00</td>
+                            <td></td>
+                            <td>₹ .00</td>
+                        </tr>
 
                                
                                 </tbody>
@@ -289,37 +308,33 @@ async function getuserInfoRealtime(userID){
             </div>
         </div>
                        
-                        `
-                        editProfile["name"].value = userInfo.name
-                        editProfile["profileEmail"].value = userInfo.email
-                        editProfile["regno"].value = userInfo.regno
-                        editProfile["phoneno"].value = userInfo.phone
-                        editProfile["whatsapp"].value = userInfo.whatsapp
-                        editProfile["blood"].value = userInfo.blood
-                        editProfile["department"].value = userInfo.department
-                        editProfile["department2"].value = userInfo.department2
-                        editProfile["college"].value = userInfo.college
-                        editProfile["address"].value = userInfo.address
-                        editProfile["state"].value = userInfo.state
-                        editProfile["pincode"].value = userInfo.pincode
-                        editProfile["exp"].value = userInfo.exp
+                        `;
+          editProfile["name"].value = userInfo.name;
+          editProfile["profileEmail"].value = userInfo.email;
+          editProfile["regno"].value = userInfo.regno;
+          editProfile["phoneno"].value = userInfo.phone;
+          editProfile["whatsapp"].value = userInfo.whatsapp;
+          editProfile["blood"].value = userInfo.blood;
+          editProfile["department"].value = userInfo.department;
+          editProfile["department2"].value = userInfo.department2;
+          editProfile["college"].value = userInfo.college;
+          editProfile["address"].value = userInfo.address;
+          editProfile["state"].value = userInfo.state;
+          editProfile["pincode"].value = userInfo.pincode;
+          editProfile["exp"].value = userInfo.exp;
 
-
-                        if(firebase.auth().currentUser.photoURL){
-                            document.querySelector('#proimg').src = firebase.auth().currentUser.photoURL
-                        }
-                      
-
-                }    
-             }
-        })
-
-
-    }else{
-        userDetails.innerHTML = ` <div class="container-fluid" style="max-width: 80vh;margin-top:180px">
-        <div class="card" style="border-top: 3px solid #198754">
+          if (firebase.auth().currentUser.photoURL) {
+            document.querySelector("#proimg").src =
+              firebase.auth().currentUser.photoURL;
+          }
+        }
+      }
+    });
+  } else {
+    userDetails.innerHTML = ` <div class="container-fluid" style="max-width: 80vh;margin-top:180px">
+        <div class="card" style="border-top: 3px solid rgb(53, 53, 53)">
         <div class="card-body" style="text-align: center">
-        <h5 class="card-title" style="text-align: center; color: #198754; font-weight: 700; font-size:25px">MEMBERS PORTAL</h5><hr>
+        <h5 class="card-title" style="text-align: center; color: rgb(53, 53, 53); font-weight: 700; font-size:25px">MEMBERS PORTAL</h5><hr>
 <br>
         <form autocomplete="off" onsubmit="login(event)">
             <input type="email" class="form-control" id="loginEmail" placeholder="Email" style="width: 98%;
@@ -328,9 +343,9 @@ async function getuserInfoRealtime(userID){
             <input type="password" class="form-control" id="loginPassword" placeholder="Password" style="width: 98%;margin-top:10px;
             height: 40px;padding-left:8px;font-size: 13px;border: 1px solid #e8e8e8; border-radius:5px">
             <br>
-            <div style="text-align:left"> <a style="text-decoration: none; color: #198754; font-size: 12px; margin-left: 10px" class="modal-trigger" href="#modal2">Forgot Password</a></div>
+            <div style="text-align:left"> <a style="text-decoration: none; color: rgb(53, 53, 53); font-size: 12px; margin-left: 10px" class="modal-trigger" href="#modal2">Forgot Password</a></div>
             <br> 
-            <button type="submit" class="btn" style="background-color:#198754;color: #fff; padding-bottom:8px; float: left"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</button>
+            <button type="submit" class="btn" style="background-color:rgb(53, 53, 53);color: #fff; padding-bottom:8px; float: left"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</button>
             
     </form>
           <div>
@@ -350,11 +365,6 @@ async function getuserInfoRealtime(userID){
     <script src="assets/js/Chart.bundle.js"></script>
     <script src="assets/js/chart.js"></script>
     <script src="assets/js/app.js"></script>
-        `
-    }
+        `;
+  }
 }
-
-
-
-
-
